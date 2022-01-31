b108962d4726d2a15e7606031fc0d4777efcef30
david77 <69095207+davve77@users.noreply.github.com>
2022-01-31 23:54:09 +0200

    Update plugin.bwrd.js

--- plugins/plugin.bwrd.js
+++ plugins/plugin.bwrd.js
@@ -3,7 +3,7 @@
     @version 1.0.0
     @description My cool plugin.
     @author You
-    @source https://raw.githubusercontent.com/davve77/testing/main/firstPlugin.bwrd.js
+    @source https://raw.githubusercontent.com/davve77/BetterWRD-Plugins/main/plugins/plugin.bwrd.js
 */
 
 
